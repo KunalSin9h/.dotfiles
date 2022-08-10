98190bf037d76993421212accd23e488d7511c46
Kunal Singh <kunalsin9h@gmail.com>
2022-08-10 12:31:33 +0530

    add fish

---
+++ fish/.config/fish/alias.fish
@@ -0,0 +1,32 @@
+# apps... but better
+alias git=hub
+alias ls=lsd
+alias cat='batcat'
+
+# git
+abbr -a gs  git status -sb
+abbr -a ga  git add
+abbr -a gc  git commit
+abbr -a gcm git commit -m
+abbr -a gca git commit --amend
+abbr -a gcl git clone
+abbr -a gco git checkout
+abbr -a gp  git push
+abbr -a gpl git pull
+abbr -a gl  git l
+abbr -a gd  git diff
+abbr -a gds git diff --staged
+abbr -a gr  git rebase -i HEAD~15
+abbr -a gf  git fetch
+abbr -a gfc git findcommit
+abbr -a gfm git findmessage
+abbr -a gco git checkout
+
+# ls
+alias l='lsd -l'
+alias la='lsd -a'
+alias lla='lsd -la'
+alias lt='lsd --tree'
+
+# misc
+alias reload='exec fish'
