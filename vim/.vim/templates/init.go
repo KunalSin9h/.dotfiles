0e8af9815a6961a2074c0cf53fb943fb548fb8f8
Kunal Singh <kunalsin9h@gmail.com>
2023-12-04 15:58:21 +0530

    update go template

--- vim/.vim/templates/init.go
+++ vim/.vim/templates/init.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "bufio"
+    "os"
+)
 
 func main(){
 
+}
+
+func getScanner() *bufio.Scanner {
+    var inputFile string
+
+    flag.StringVar(&inputFile, "input", "input.txt", "The input file")
+    flag.Parse()
+
+    file, err := os.Open(inputFile)
+    if err != nil {
+        panic(err)
+    }
+
+    return bufio.NewScanner(file)
 }
